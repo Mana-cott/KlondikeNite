9e4d892ff7a999da79c3efc18c5dd5d73f76746f
Jack Hannan <jackhannan2k353@gmail.com>
2025-01-16 18:17:09 -0500

    Add Basic UI Control

---
+++ scripts/menu/modeselect.gd
@@ -0,0 +1,43 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_player_pressed():
+	pass # Replace with function body.
+
+
+func _on_one_player_pressed():
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+
+func _on_two_player_pressed():
+	pass # Replace with function body.
+
+
+func _on_three_player_pressed():
+	pass # Replace with function body.
+
+
+func _on_four_player_pressed():
+	pass # Replace with function body.
+
+
+func _on_five_player_pressed():
+	pass # Replace with function body.
+
+
+func _on_six_player_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_pressed():
+	get_tree().change_scene_to_file("res://scenes/menus/menu.tscn")
